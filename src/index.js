8dfa462936c2bb84e417b80132daef2d8bd207b2
Jonathan Lafleur <me@jonathanlafleur.ca>
2017-09-11 13:40:07 -0400

    Changed path to index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
     });
     
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'src/index.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
